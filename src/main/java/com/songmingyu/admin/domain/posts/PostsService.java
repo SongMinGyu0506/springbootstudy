cd32be9deb7e86c40dfaae74d6f93f37a701e75e
SongMinGyu0506 <ggp04114@naver.com>
2021-07-03 23:24:45 +0900

    STUDY
    Difference Server Template Engine and Client Template Enginge
    How to use Mustache
    Spring boot display
    loading speed

--- src/main/java/com/songmingyu/admin/domain/posts/PostsService.java
+++ src/main/java/com/songmingyu/admin/domain/posts/PostsService.java
@@ -40,5 +40,11 @@ public class PostsService {
         return postRepository.findALlDesc().stream().map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
     }
+    
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postRepository.delete(posts);
+    }
 
 }
--- src/main/java/com/songmingyu/admin/web/PostsApiController.java
+++ src/main/java/com/songmingyu/admin/web/PostsApiController.java
@@ -26,6 +26,12 @@ public class PostsApiController {
     public PostsResponseDto findById (@PathVariable Long id) {
         return postsService.findById(id);
     }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
 }
 /*Controller와 Service에서 Autowired가 없는 이유?
 * Spring에서 Bean을 주입받는 방식
