0d812e657f4888d12644cdc63dfc519ee93acd55
songmingyu0506 <ggp04114@naver.com>
2021-07-09 19:16:00 +0900

    Modify TestCode

--- src/test/java/com/songmingyu/admin/web/HelloControllerTest.java
+++ src/test/java/com/songmingyu/admin/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.songmingyu.admin.web;
 
+import com.songmingyu.admin.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -12,19 +16,27 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
+
 /*
+*   @RunWith(SpringRunner.class)
 *   테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킨다.
 *   여기서는 SpringRunner라는 스프링 실행자를 사용한다.
 *   즉, 스프링 부트 테스트와 JUnit 사이에 연결자 역할을 한다.
 */
-@WebMvcTest(controllers = HelloController.class)
+
 /*
+*   @WebMvcTest(controllers = HelloController.class)
 *   여러 스프링 테스트 어노테이션 중, Web(Spring MVC)에 집중할 수 있는 어노테이션
 *   선언할 경우 @Controller @ControllerAdvice 등을 사용할 수 있다.
 *   단, @Service, @Component, @Repository 등은 사용할 수 없다.
 *   여기서는 컨트롤러만 사용하기 때문에 선언
 */
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class,
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+    }
+)
 public class HelloControllerTest {
     @Autowired //Spring이 관리하는 Bean을 주입받음
     private MockMvc mvc;
@@ -34,6 +46,7 @@ public class HelloControllerTest {
     *   이 클래스를 통해 HTTP GET,POST 등에 대한 API 테스트 가능
     */
 
+    @WithMockUser(roles = "USER")
     @Test
     public void Return_Hello() throws Exception {
         String hello = "hello";
@@ -55,7 +68,7 @@ public class HelloControllerTest {
                 * Controller에서 "hello" 리턴하므로 맞는지 검증
                 * */
     }
-
+    @WithMockUser(roles = "USER")
     @Test
     public void Return_HelloresponseDto() throws  Exception {
         String name = "hello";
